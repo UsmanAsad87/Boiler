284a3db2ec810dbe7fe8eb93fd283e0ad40fec1c
Muhammad Saqib <224saqib@gmail.com>
2022-09-16 11:28:12 +0500

    changes

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:boiler/view/SplashScreen/splashScreen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        designSize: const Size(390, 844),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false, home: SplashScreen()
+              // CompleteMcsBenchMark()
+              // CustomerSatisficationQuestionair()
+              // PostInstallationAshp()
+              // InstallMenuOfAshp()
+              // CustomerDetailForAshp()
+              // RequiredPicture()
+              // ElectricalSystem()
+              // SplashScreen()
+              );
+        });
+  }
+}
